35b132c80f246cf150c6a8596ed5e492f9a47f30
chase.fbls <113160423+dmitriypudov@users.noreply.github.com>
2023-05-31 18:41:22 +0700

    Add files via upload

---
+++ laba13_pudov/laba13_pudov/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace laba13_pudov
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn = new System.Windows.Forms.Button();
+            this.TengeEdit = new System.Windows.Forms.NumericUpDown();
+            this.RubleEdit = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.TengeEdit)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RubleEdit)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.btn);
+            this.panel1.Controls.Add(this.TengeEdit);
+            this.panel1.Controls.Add(this.RubleEdit);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(3, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(159, 398);
+            this.panel1.TabIndex = 0;
+            // 
+            // btn
+            // 
+            this.btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn.Location = new System.Drawing.Point(12, 113);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(129, 30);
+            this.btn.TabIndex = 4;
+            this.btn.Text = "Старт";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // TengeEdit
+            // 
+            this.TengeEdit.DecimalPlaces = 2;
+            this.TengeEdit.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TengeEdit.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.TengeEdit.Location = new System.Drawing.Point(68, 57);
+            this.TengeEdit.Maximum = new decimal(new int[] {
+            250,
+            0,
+            0,
+            0});
+            this.TengeEdit.Name = "TengeEdit";
+            this.TengeEdit.Size = new System.Drawing.Size(73, 24);
+            this.TengeEdit.TabIndex = 3;
+            this.TengeEdit.Value = new decimal(new int[] {
+            553,
+            0,
+            0,
+            131072});
+            // 
+            // RubleEdit
+            // 
+            this.RubleEdit.DecimalPlaces = 2;
+            this.RubleEdit.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.RubleEdit.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.RubleEdit.Location = new System.Drawing.Point(68, 21);
+            this.RubleEdit.Maximum = new decimal(new int[] {
+            250,
+            0,
+            0,
+            0});
+            this.RubleEdit.Name = "RubleEdit";
+            this.RubleEdit.Size = new System.Drawing.Size(73, 24);
+            this.RubleEdit.TabIndex = 2;
+            this.RubleEdit.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(9, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Тенге";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(9, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Рубль";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.chart1);
+            this.panel2.Location = new System.Drawing.Point(168, 2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(487, 398);
+            this.panel2.TabIndex = 1;
+            // 
+            // chart1
+            // 
+            this.chart1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(0, 0);
+            this.chart1.Name = "chart1";
+            series1.BorderWidth = 2;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.IsValueShownAsLabel = true;
+            series1.LabelFormat = "C";
+            series1.Legend = "Legend1";
+            series1.Name = "Рубль";
+            series2.BorderWidth = 2;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.IsValueShownAsLabel = true;
+            series2.LabelFormat = "C";
+            series2.Legend = "Legend1";
+            series2.Name = "Тенге";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(484, 395);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(657, 401);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.TengeEdit)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RubleEdit)).EndInit();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.NumericUpDown TengeEdit;
+        private System.Windows.Forms.NumericUpDown RubleEdit;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ laba13_pudov/laba13_pudov/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace laba13_pudov
+{
+    public partial class Form1 : Form // объявляем класс формы
+    {
+        public Form1() // конструктор класса формы
+        {
+            InitializeComponent(); // инициализация компонентов формы
+        }
+
+        const double m = 0, sd = 0.015, k = m - 0.5 * sd * sd; // объявление и инициализация констант
+
+        double tenge, rubles, day, boxG, temp, temp2; // объявление переменных
+
+        private void timer1_Tick_1(object sender, EventArgs e) // функция-обработчик тика таймера
+        {
+            chart1.ChartAreas[0].AxisX.Maximum = Math.Ceiling(day); // устанавливаем максимальное значение оси X на графике
+
+            Random rand = new Random(); // создаем объект генератора случайных чисел
+            temp = rand.NextDouble(); // генерируем случайное число от 0 до 1
+            temp2 = rand.NextDouble(); // генерируем еще одно случайное число от 0 до 1
+            boxG = Math.Sqrt(-2.0 * Math.Log(temp)) * Math.Cos(2.0 * Math.PI * temp2); // вычисляем значение по формуле Бокса-Мюллера
+
+            tenge = tenge * Math.Exp(k + sd * boxG); // вычисляем новое значение курса 
+            rubles = rubles * Math.Exp(k + sd * boxG); // вычисляем новое значение курса 
+
+            chart1.Series[0].Points.AddXY(day, rubles); // добавляем новую точку на график 
+            chart1.Series[1].Points.AddXY(day, tenge); // добавляем новую точку на график 
+            day++; // увеличиваем количество дней на 1
+        }
+
+        private void btn_Click(object sender, EventArgs e) // функция-обработчик нажатия на кнопку
+        {
+            day = 1; // сбрасываем количество дней в ноль
+            tenge = (double)RubleEdit.Value; // инициализируем начальное значение курса 
+            rubles = (double)TengeEdit.Value; // инициализируем начальное значение курса 
+
+            chart1.Series[0].Points.Clear(); // очищаем данные на графике 
+            chart1.Series[0].Points.AddXY(0, rubles); // добавляем начальную точку на график 
+            chart1.Series[1].Points.Clear(); // очищаем данные на графике 
+            chart1.Series[1].Points.AddXY(0, tenge); // добавляем начальную точку на график 
+
+            timer1.Enabled = !timer1.Enabled; // запускаем или останавливаем таймер
+            if (timer1.Enabled == false) // если таймер остановлен
+            {
+                btn.Text = "Старт"; // меняем текст кнопки на "Старт"
+            }
+            else // иначе
+            {
+                btn.Text = "Стоп"; // меняем текст кнопки на "Стоп"
+            }
+        }
+    }
+}
